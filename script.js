cca6dc7c5c66f3590f3e57f94424a7de91069a80
Somnath Choudhury <102171721+somnath-choudhury@users.noreply.github.com>
2025-08-05 15:46:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function isSameType(value1, value2) {
   //your js code here
-	if (typeof(value1) === typeof(value2))
-		return true
-	return false
+	 if (Number.isNaN(value1) && Number.isNaN(value2)) {
+    return true;
+  }
+
+
+  return typeof value1 === typeof value2;
 }
 
 // do not change the code below.
